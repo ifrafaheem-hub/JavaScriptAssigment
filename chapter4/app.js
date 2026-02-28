3489e57ce952a16f58af44c92537ce281e2bab62
ifrafaheem-hub <ifrafaheem17@gmail.com>
2026-02-28 14:01:05 +0500

    chapter4

---
+++ chapter4/app.js
@@ -0,0 +1,32 @@
+//Question 1
+let one="1" , two="2" , three="3"
+
+Question 2
+//5 legal variables
+let userName
+let -script
+let $name 
+let student_age
+
+//5 illegal variables
+let first name 
+let @script
+let student-age
+let name$
+let let 
+
+//a
+let jsVariables = "<b>Rules of naming JS variables</b><br><br>" +
+"Variable names can only contain numbers, $, and _. For example: $my_1stVariable.<br>" +
+"Variable must begin with a letter, $, or _. For example: $name, _name, or name.<br>" +
+"Variable names are case sensitive.<br>" +
+"Variable names should not be JS keywords.<br>";
+
+document.write(jsVariables)
+
+
+
+
+
+
+
