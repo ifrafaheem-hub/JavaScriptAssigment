bf0c4b3c7e573faf500d0d40fbf0b5a444f0d190
ifrafaheem-hub <ifrafaheem17@gmail.com>
2026-02-28 13:22:53 +0500

    chapter3

---
+++ chapter3/app.js
@@ -0,0 +1,16 @@
+// Question 1
+let age="I am"+" 18 "+ "years old"
+alert(age)
+
+//Question 2
+let track="You have visited this site "+"14 "+"times"
+alert(track)
+
+//Question 3
+let birthyear="My birth year is "+ "2008" + "<br>" + 
+"Data type of my ddeclared variable is number"
+document.write(birthyear)
+
+//Question 4
+let clothing="<b>John Doe</b> ordered <b>5 T-shirts</b>(s) on XYZ clothing store"
+document.write(clothing)
