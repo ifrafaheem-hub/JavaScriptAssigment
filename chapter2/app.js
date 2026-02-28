98be52ddd271f7a7da61aa369887164c3e6c3ec8
ifrafaheem-hub <ifrafaheem17@gmail.com>
2026-02-28 12:36:38 +0500

    chap2

---
+++ chapter2/app.js
@@ -0,0 +1,38 @@
+// Question no 1
+var username
+
+// Question no 2
+var myName ="Ifra Faheem"
+
+
+// Question no 3
+var message="Hello World"
+alert(message)
+
+// Question no 4
+var studentName="Jhone Doe"
+alert(studentName)
+var Age="15 " + "years old"
+alert(Age)
+var bioData="Certified Mobile Application Development"
+alert(bioData)
+
+// Question no 5
+var food=" PIZZA \n PIZZ \n PIZ \n PI \n P"
+alert(food)
+
+// Question no 6
+let email="ifrafaheem@gmail.com"
+alert("My email adress is "  + email)
+
+//Question no 7
+let book=" A Smarter way to learn Javascript"
+alert("Im trying to learn from the Book " + book)
+
+// Question no 8
+let script="Yah! I can write HTML content through Javascript"
+document.write(script)
+
+//Question 9
+let border="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(border)
